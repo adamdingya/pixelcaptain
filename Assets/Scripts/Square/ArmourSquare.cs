014c16b569b7e59afe4637dfb7058a99cdc4e762
Ding Ya <adamdingya@hotmail.com>
2015-12-28 09:15:29 +0000

    Moving squares to generic folder

--- Assets/Scripts/Square/ArmourSquare.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ArmourSquare : Square {
-	
-	public ArmourSquare(float mass, float health, string leftUUID, string rightUUID, string upUUID, string downUUID) : base (mass, health, leftUUID, rightUUID, upUUID, downUUID){
-
-	}
-}
