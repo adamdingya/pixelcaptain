91f4932e9be1c0cb1640dff0035397de284e91cb
Ding Ya <adamdingya@hotmail.com>
2015-12-28 09:26:47 +0000

    adding in new square class

---
+++ Assets/Scripts/Generic/ArmourSquare.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmourSquare : Square {
+	
+	public ArmourSquare(float mass, float health, string leftUUID, string rightUUID, string upUUID, string downUUID) : base (mass, health, leftUUID, rightUUID, upUUID, downUUID){
+
+	}
+}
---
+++ Assets/Scripts/Generic/PowerSquare.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerSquare : Square {
+
+	private float capacity;
+
+	public PowerSquare(float mass, float health, string leftUUID, string rightUUID, string upUUID, string downUUID) : base (mass, health, leftUUID, rightUUID, upUUID, downUUID) {
+		
+	}
+
+	public PowerSquare(float mass, float health, string leftUUID, string rightUUID, string upUUID, string downUUID, float capacity) : base (mass, health, leftUUID, rightUUID, upUUID, downUUID) {
+		this.capacity = capacity;
+	}
+	
+	public void setCapacity(float capacity){
+		this.capacity = capacity;
+	}
+	
+	public float getCapacity(){
+		if(capacity==0f){
+			return DefaultValues.DEFAULT_POWER_CAPACITY;
+		}
+		return capacity;
+	}
+}
